a86a537b5fa68dd51afe150c33daacf27de76b7b
jordang7 <77jgreenberg77@gmail.com>
2021-02-12 18:29:35 -0500

    added display of ingredients and their amounts

--- src/cocktails.js
+++ src/cocktails.js
@@ -5,13 +5,22 @@ import axios from 'axios';
 
 export default class CocktailList extends React.Component {
     state = {
-      cocktails: [],
-      ingredients:[]
+      cocktails: []
     }
 
     componentDidMount() {
         axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a").then(res=>{
         const cocktails = res.data.drinks;
+        for(var i=0;i<cocktails.length;i++){
+            var drink = cocktails[i];
+            let index = 1;
+            let ingredientArray = [];
+            while (drink['strIngredient' + index]) {
+                ingredientArray.push({name: drink['strIngredient' + index], amount: drink['strMeasure' + index] ? drink['strMeasure' + index]: "A dash "});
+                index++;
+            }
+            drink.ingredientArray=ingredientArray
+    }
         this.setState({ cocktails });
             }).catch(error =>console.error(`Error: ${error}`));
     }
@@ -23,7 +32,10 @@ export default class CocktailList extends React.Component {
         <ul>
           { (this.state.cocktails).map(cocktails => <li key={cocktails.strDrink}> {cocktails.strDrink} <ul>
               <li>Glass: {cocktails.strGlass}</li>
-              <li>
+              <li>Ingredients:
+                <ul>{ (cocktails.ingredientArray).map(ingredient => <li>{ingredient.amount} of {ingredient.name}</li>)}</ul>
+                </li>
+                <li>
                   Instructions:{cocktails.strInstructions}
               </li>
           </ul>
